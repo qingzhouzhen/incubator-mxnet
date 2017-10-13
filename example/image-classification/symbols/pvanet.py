7361e794e9f1abfc519612bda5ff673aef8154fa
huanghanqing <huanghanqing4@huawei.com>
2017-10-13 17:30:46 +0800

    pvanet as zhengzhengyu, confirm params

--- example/image-classification/symbols/pvanet.py
+++ example/image-classification/symbols/pvanet.py
@@ -22,8 +22,7 @@ def get_symbol(num_classes, **kwargs):
     conv5_3 = inception(data=conv5_2, middle_filter=[64, [96, 192], [32, 64, 64]], num_filter=384, kernel=(3, 3), stride=(1, 1), proj=False, last=False, name='conv5_3', suffix='') #(6*6)x384/(6*6)x384
     conv5_4 = inception(data=conv5_3, middle_filter=[64, [96, 192], [32, 64, 64]], num_filter=384, kernel=(3, 3), stride=(1, 1), proj=False, last=True, name='conv5_4', suffix='') #(6*6)x384/(6*6)x384
     bn_relu = bn_and_relu(data=conv5_4, name='bn_and_relu', suffix='last')
-    pool5 = mx.sym.Pooling(data=bn_relu, kernel=(1, 1), stride=(1, 1), pad=(0, 0), pool_type='max', name='pool5')
-    flatten = mx.sym.Flatten(data=pool5, name='flatten')
+    flatten = mx.sym.Flatten(data=bn_relu, name='flatten')
     fc6 = fullconnection(data=flatten, num_hidden=4096, name='fc6', suffix='')
     fc7 = fullconnection(data=fc6, num_hidden=4096, name='fc7', suffix='')
     fc8 = mx.sym.FullyConnected(data=fc7, num_hidden=num_classes, name='fc8')
@@ -32,10 +31,10 @@ def get_symbol(num_classes, **kwargs):
 
 def crelu(data, num_filter, kernel, stride, pad, name=None, suffix=''):
     conv1=mx.symbol.Convolution(data=data, num_filter=num_filter, kernel=kernel, stride=stride, pad=pad, no_bias=True, name='%s%s_conv2d' %(name, suffix))
-    bn=mx.symbol.BatchNorm(data=conv1, name='%s%s_batchnorm' %(name, suffix))
+    bn=mx.symbol.BatchNorm(data=conv1, fix_gamma=True, name='%s%s_batchnorm' %(name, suffix))
     negative=mx.symbol.negative(data=bn, name='negative')
     concat=mx.symbol.concat(bn, negative)
-    net=scale_and_shift(concat, num_filter)
+    net=scale_and_shift(concat, name=name, suffix='scale')
     act=mx.symbol.Activation(data=net, act_type='relu', name='%s%s_relu' %(name, suffix))
     return act
 
@@ -54,14 +53,14 @@ def res_crelu(data, middle_filter, num_filter, kernel, stride, pad, proj, name=N
         shortcut = data
         data = mx.sym.BatchNorm(data=data,name='%s%s_shortcut_batchnorm' %(name, suffix))
         data = mx.sym.Activation(data=data, act_type='relu', name='%s%s_shortcut_relu' % (name, suffix))
-    conv1 = Conv(data=data, num_filter=middle_filter[0], kernel=(1, 1), stride=(1, 1), pad=(0, 0), name='%s%s_1_con2d' %(name, suffix))
+    conv1 = Conv(data=data, num_filter=middle_filter[0], kernel=(1, 1), stride=(1, 1), pad=(0, 0), name='%s%s_1' %(name, suffix))
     #crelu
-    conv2 = mx.sym.Convolution(data=conv1, num_filter=middle_filter[1], kernel=kernel, stride=stride, pad=pad, name='%s%s_2_con2d' %(name, suffix))
+    conv2 = mx.sym.Convolution(data=conv1, num_filter=middle_filter[1], kernel=kernel, stride=stride, pad=pad, no_bias=True, name='%s%s_2_con2d' %(name, suffix))
     bn = mx.sym.BatchNorm(data=conv2, name='%s%s_batchnorm' %(name, suffix))
     negative = mx.sym.negative(data=bn, name='%s%s_negative'%(name, suffix))
     concat = mx.sym.concat(bn, negative)
     scale = scale_and_shift(data=concat, name=name, suffix=suffix)
-    relu = mx.sym.Activation(data=scale, act_type='relu', name='%s%s_relu' %(name, suffix))
+    relu = mx.sym.Activation(data=scale, act_type='relu', name='%s%s_3_relu' %(name, suffix))
 
     conv3 = mx.sym.Convolution(data=relu, num_filter=num_filter, kernel=(1, 1), stride=(1, 1), pad=(0, 0), name='%s%s_3_con2d' %(name, suffix))
     act = conv3+shortcut
@@ -77,19 +76,19 @@ def inception(data, middle_filter, num_filter, kernel, stride, proj, last, name,
         shortcut = mx.sym.Convolution(data=data, num_filter=num_filter, kernel=(1, 1), stride=stride, name='%s_proj' %(name))
     else:
         shortcut = data
-    bn_relu = bn_and_relu(data=data, name=name, suffix='bsr')
-    conv_a = Conv(data=bn_relu, num_filter=middle_filter[0], kernel=(1, 1), stride=stride, pad=(0, 0), name=name, suffix='a')
+    bn_relu = bn_and_relu(data=data, name=name, suffix='_bn_and_relu')
+    conv_a = Conv(data=bn_relu, num_filter=middle_filter[0], kernel=(1, 1), stride=stride, pad=(0, 0), name=name, suffix='_a')
 
-    conv_b1 = Conv(data=bn_relu, num_filter=middle_filter[1][0], kernel=(1, 1), stride=stride, pad=(0, 0), name=name, suffix='b1')
-    conv_b2 = Conv(data=conv_b1, num_filter=middle_filter[1][1], kernel=kernel, stride=(1, 1), pad=(1, 1), name=name, suffix='b2')
+    conv_b1 = Conv(data=bn_relu, num_filter=middle_filter[1][0], kernel=(1, 1), stride=stride, pad=(0, 0), name=name, suffix='_b1')
+    conv_b2 = Conv(data=conv_b1, num_filter=middle_filter[1][1], kernel=kernel, stride=(1, 1), pad=(1, 1), name=name, suffix='_b2')
 
-    conv_c1 = Conv(data=bn_relu, num_filter=middle_filter[2][0], kernel=(1, 1), stride=stride, pad=(0, 0), name=name, suffix='c1')
-    conv_c2 = Conv(data=conv_c1, num_filter=middle_filter[2][1], kernel=kernel, stride=(1, 1), pad=(1, 1), name=name, suffix='c2')
-    conv_c3 = Conv(data=conv_c2, num_filter=middle_filter[2][2], kernel=kernel, stride=(1, 1), pad=(1, 1), name=name, suffix='c3')
+    conv_c1 = Conv(data=bn_relu, num_filter=middle_filter[2][0], kernel=(1, 1), stride=stride, pad=(0, 0), name=name, suffix='_c1')
+    conv_c2 = Conv(data=conv_c1, num_filter=middle_filter[2][1], kernel=kernel, stride=(1, 1), pad=(1, 1), name=name, suffix='_c2')
+    conv_c3 = Conv(data=conv_c2, num_filter=middle_filter[2][2], kernel=kernel, stride=(1, 1), pad=(1, 1), name=name, suffix='_c3')
 
     if stride[1] > 1:
-        pool_d = mx.sym.Pooling(data=bn_relu, kernel=kernel, stride=stride, pad=(1, 1), pool_type='max', name=name+'pool')
-        conv_d = Conv(data=pool_d, num_filter=middle_filter[3], kernel=(1, 1), stride=(1, 1), pad=(0, 0), name=name, suffix='proj_conv')
+        pool_d = mx.sym.Pooling(data=bn_relu, kernel=kernel, stride=stride, pad=(1, 1), pool_type='max', name=name+'_pool')
+        conv_d = Conv(data=pool_d, num_filter=middle_filter[3], kernel=(1, 1), stride=(1, 1), pad=(0, 0), name=name, suffix='_d')
         conv_concat = mx.sym.concat(conv_a, conv_b2, conv_c3, conv_d)
     else:
         conv_concat = mx.sym.concat(conv_a, conv_b2, conv_c3)
@@ -102,12 +101,12 @@ def inception(data, middle_filter, num_filter, kernel, stride, proj, last, name,
     return output
 
 def Conv(data, num_filter=1, kernel=(1, 1), stride=(1, 1), pad=(0, 0), name=None, suffix=''):
-    conv = mx.sym.Convolution(data=data, num_filter=num_filter, kernel=kernel, stride=stride, pad=pad, name='%s%s_conv2d' %(name, suffix))
+    conv = mx.sym.Convolution(data=data, num_filter=num_filter, kernel=kernel, stride=stride, pad=pad, no_bias=True, name='%s%s_conv2d' %(name, suffix))
     bn = mx.sym.BatchNorm(data=conv, name='%s%s_batchnorm' %(name, suffix))
     act = mx.sym.Activation(data=bn, act_type='relu', name='%s%s_relu' %(name, suffix))
     return act
 def fullconnection(data, num_hidden, name, suffix):
-    fc = mx.sym.FullyConnected(data=data, num_hidden=num_hidden, name='%s' %(name))
+    fc = mx.sym.FullyConnected(data=data, num_hidden=num_hidden, no_bias=True, name='%s' %(name))
     bn = mx.sym.BatchNorm(data=fc, name='%s%s_batchnorm' %(name, suffix))
     dropout = mx.sym.Dropout(data=bn, p=0.5, name= '%s_dropout' %(name))
     relu = mx.sym.Activation(data=dropout, act_type='relu', name='%s_relu' %(name))
